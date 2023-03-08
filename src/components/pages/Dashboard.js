5060e6643f326b83d0341acf2cbee39396251659
Yash Gaherwar <yashgaherwar2002@gmail.com>
2023-03-08 16:55:10 +0530

    responsive navbar added

---
+++ src/components/pages/Dashboard.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Dashboard() {
+  return <h1 className='dashboard'>Dashboard</h1>;
+}
--- src/index.js
+++ src/index.js
@@ -5,15 +5,28 @@ import Login from './Pages/Login/Login';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import Dashboard from './components/pages/Dashboard';
+import AddVoters from './components/pages/AddVoters';
+import ContactUs from './components/pages/ContactUs';
+import SignUp from './components/pages/SignUp';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
     <Router>
+      <Navbar/>
       <Routes>
         <Route exact path='/' element={<App/>}/>
         <Route exact path='/login' element={<Login/>}/>
+        <Route exact path='/home' element={<Home/>} />
+        <Route exact path='/dashboard' element={<Dashboard/>} />
+        <Route exact path='/addvoters' element={<AddVoters/>} />
+        <Route exact path='/contact-us' element={<ContactUs/>} />
+        <Route exact path='/sign-up' element={<SignUp/>} />
       </Routes>
     </Router>
     </>
