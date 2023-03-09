4d33256f7e28f55cf77558a337f941911708393b
devanshukd04 <devanshukd27@gmail.com>
2023-03-10 00:43:47 +0530

    Added filtering based on multiple fields

--- src/Pages/QueryingPage/QueryingPage.js
+++ src/Pages/QueryingPage/QueryingPage.js
@@ -13,33 +13,56 @@ const QueryingPage = () => {
   const keys = [key];
   const [data, setData] = useState([]);
   const [pageNo, setPageNo] = useState(1);
+  const [firstName,setFirstName]=useState("");
+  const [middleName,setMiddleName]=useState("");
+  const [lastName,setLastName]=useState("");
+  const [age,setAge]=useState("");
+  const [ward,setWard]=useState("");
+  const [voterId,setVoterId]=useState("");
 
   useEffect(() => {
     getData();
-  }, [query]);
+  });
 
   const getData = async (e) => {
-    await axios
-      .get(`http://localhost:8080/user/`)
+    console.log("FirstName "+firstName+" Middle Name "+middleName+" Last Name "+lastName+" age "+age+" Ward "+ward+" VoterId "+voterId);
+    if(age==""){
+      console.log("Age");
+      await axios
+      .get(`http://localhost:8080/user/get?voterId=${voterId}&firstName=${firstName}&middleName=${middleName}&lastName=${lastName}&gender&age=-1&district&city&ward=${ward}&pageNo=0`)
       .then((response) => {
-        setData(Search(response.data));
+        console.log(response);
+        console.log(response.data.totalPages);
+        console.log(response.data.voterList);
+        setData(response.data.voterList);
       })
       .catch(function (error) {
         console.log(error);
       });
+    }
+    else{
+      await axios
+      .get(`http://localhost:8080/user/get?voterId=${voterId}&firstName=${firstName}&middleName=${middleName}&lastName=${lastName}&gender&age=${age}&district&city&ward=${ward}&pageNo=0`)
+      .then((response) => {
+        setData(response.data.voterList);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
   };
 
-  const selectOption = async (val) => {
-    await getData();
-    setkey(val);
-    setData(Search(data));
-  };
+  // const selectOption = async (val) => {
+  //   await getData();
+  //   setkey(val);
+  //   setData(Search(data));
+  // };
 
-  const Search = (data) => {
-    return data.filter((item) =>
-      keys.some((key) => item[key].toLowerCase().startsWith(query))
-    );
-  };
+  // const Search = (data) => {
+  //   return data.filter((item) =>
+  //     keys.some((key) => item[key].toLowerCase().startsWith(query))
+  //   );
+  // };
 
   const firstEvent = (e) => {
     var bottom =
@@ -53,7 +76,7 @@ const QueryingPage = () => {
 
   return (
     <div className="app">
-      <label className="dropDown">
+      {/* <label className="dropDown">
         Filter By:
         <select
           className="select"
@@ -69,9 +92,9 @@ const QueryingPage = () => {
 
           <option value="ward">Ward</option>
         </select>
-      </label>
+      </label> */}
 
-      <input
+      {/* <input
         className="search"
         placeholder="Search Voter"
         onChange={(e) => {
@@ -79,10 +102,22 @@ const QueryingPage = () => {
             setData(Search(data));
           });
         }}
-      />
+      /> */}
 
       <br></br>
 
+      <div class="search-container">
+        <input type="text" class="search" placeholder="First Name" onChange={e=>setFirstName(e.target.value)}/>
+        <input type="text" class="search" placeholder="Middle Name" onChange={e=>setMiddleName(e.target.value)}/>
+        <input type="text" class="search" placeholder="Last Name" onChange={e=>setLastName(e.target.value)}/>
+      </div>
+      <div class="search-container">
+        <input type="text" class="search" placeholder="Age" onChange={e=>setAge(e.target.value)}/>
+        <input type="text" class="search" placeholder="Ward" onChange={e=>setWard(e.target.value)}/>
+        <input type="text" class="search" placeholder="Voter ID" onChange={e=>setVoterId(e.target.value)}/>
+      </div>
+      <br/>
+      <br/>
       <div class="container mx-auto" id="header">
         <div className="grid grid-cols-7 gap-4" id="whiteText">
           <div className="">VoterId</div>
