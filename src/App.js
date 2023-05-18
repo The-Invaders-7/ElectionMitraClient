cecc14c95d2ce58052bb52e7404b96f21936137c
Yash <yashgaherwar2002@gmail.com>
2023-05-18 20:14:53 +0530

    Base64 Conversion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
+import Base64ImageConverter from './Base64ImageConverter';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="app-title">Base64 to Image Converter App</h1>
+      <Base64ImageConverter />
     </div>
   );
 }
