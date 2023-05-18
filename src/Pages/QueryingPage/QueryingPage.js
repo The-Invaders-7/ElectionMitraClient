c67ff30b841da7c72b7fe5d51ebf81eebe903c4d
devanshukd04 <devanshukd27@gmail.com>
2023-05-19 01:21:02 +0530

    Added Print button

--- src/Pages/QueryingPage/QueryingPage.js
+++ src/Pages/QueryingPage/QueryingPage.js
@@ -4,12 +4,13 @@ import { Users } from "./Users";
 import "./QueryingPage.css";
 import Table from "./table";
 import Item from "./item";
+import Print from '../../Images/Download.png';
 import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
 
 const QueryingPage = () => {
   const [query, setQuery] = useState("");
-  const [key, setkey] = useState("voterId");
+  const [key, setkey] = useState("voterID");
   const keys = [key];
   const [data, setData] = useState([]);
   const [pageNo, setPageNo] = useState(1);
@@ -18,18 +19,20 @@ const QueryingPage = () => {
   const [lastName,setLastName]=useState("");
   const [age,setAge]=useState("");
   const [ward,setWard]=useState("");
-  const [voterId,setVoterId]=useState("");
+  const [voterID,setVoterID]=useState("");
+  // const [base64String, setBase64String] = useState('');
+  // const [imageUrl, setImageUrl] = useState('');
 
   useEffect(() => {
     getData();
-  },[firstName,middleName,lastName,age,voterId,ward]);
+  },[firstName,middleName,lastName,age,voterID,ward]);
 
   const getData = async (e) => {
-    console.log("FirstName "+firstName+" Middle Name "+middleName+" Last Name "+lastName+" age "+age+" Ward "+ward+" VoterId "+voterId);
+    console.log("FirstName "+firstName+" Middle Name "+middleName+" Last Name "+lastName+" age "+age+" Ward "+ward+" VoterId "+voterID);
     if(age==""){
       console.log("Age");
       await axios
-      .get(`http://localhost:8080/user/get?voterId=${voterId}&firstName=${firstName}&middleName=${middleName}&lastName=${lastName}&gender&age=-1&district&city&ward=${ward}&pageNo=0`)
+      .get(`http://localhost:8080/user/get?voterID=${voterID}&firstName=${firstName}&middleName=${middleName}&lastName=${lastName}&gender=&age=-1&imageString=&district=&city=&ward=${ward}&pageNo=0`)
       .then((response) => {
         console.log(response);
         console.log(response.data.totalPages);
@@ -42,7 +45,7 @@ const QueryingPage = () => {
     }
     else{
       await axios
-      .get(`http://localhost:8080/user/get?voterId=${voterId}&firstName=${firstName}&middleName=${middleName}&lastName=${lastName}&gender&age=${age}&district&city&ward=${ward}&pageNo=0`)
+      .get(`http://localhost:8080/user/get?voterID=${voterID}&firstName=${firstName}&middleName=${middleName}&lastName=${lastName}&gender=&age=-1&imageString=&district=&city=&ward=${ward}&pageNo=0`)
       .then((response) => {
         setData(response.data.voterList);
       })
@@ -72,6 +75,23 @@ const QueryingPage = () => {
       setPageNo(pg);
       getData();
     }
+  };
+
+  const handleConvertClick = (img) => {
+    if (img) {
+      const image = new Image();
+      image.src = `data:image/png;base64, ${img}`;
+      return image.src;
+    }
+  };
+
+  const handleDownloadClick = (img) => {
+    // console.log(img);
+    //setBase64String(img);
+    const link = document.createElement('a');
+    link.href = handleConvertClick(img);;
+    link.download = 'image.png';
+    link.click();
   };
 
   return (
@@ -114,19 +134,19 @@ const QueryingPage = () => {
       <div class="search-container">
         <input type="text" class="search" placeholder="Age" onChange={e=>setAge(e.target.value)}/>
         <input type="text" class="search" placeholder="Ward" onChange={e=>setWard(e.target.value)}/>
-        <input type="text" class="search" placeholder="Voter ID" onChange={e=>setVoterId(e.target.value)}/>
+        <input type="text" class="search" placeholder="Voter ID" onChange={e=>setVoterID(e.target.value)}/>
       </div>
       <br/>
       <br/>
       <div class="container mx-auto" id="header">
         <div className="grid grid-cols-7 gap-4" id="whiteText">
-          <div className="">VoterId</div>
+          <div className="">voterID</div>
           <div>Ward</div>
           <div>First Name</div>
           <div>Middle Name</div>
           <div>Last Name</div>
           <div>Gender</div>
-          <div>Age</div>
+          <div>Print</div>
         </div>
       </div>
 
@@ -136,13 +156,16 @@ const QueryingPage = () => {
           return (
             <div class="container mx-auto">
               <div className="grid grid-cols-7 gap-4">
-                <div className="">{item.voterId}</div>
+                <div className="">{item.voterID}</div>
                 <div>{item.ward}</div>
                 <div>{item.firstName}</div>
                 <div>{item.middleName}</div>
                 <div>{item.lastName}</div>
                 <div>{item.gender}</div>
-                <div>{item.age}</div>
+                <div><button onClick={()=>{handleDownloadClick(item.imageString)}}>
+                      <img src={Print} alt="Print Image"/>
+                    </button>
+                </div>
               </div>
             </div>
           );
